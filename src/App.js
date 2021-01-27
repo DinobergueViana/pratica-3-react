fca0a6a92ccfbf6474a78e2f833af18ba3318b19
Luiza Cintra Fernandes <luiza.fernandes@arcadis.com>
2021-01-27 09:18:10 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        div > < /div> <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
